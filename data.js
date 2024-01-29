3a134f7c5d7e1c1b2084eeb7ae8bdd7468c5439b
Beryl Marchand <158066320+Beryl-Marchand@users.noreply.github.com>
2024-01-29 16:20:23 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,27 @@
+//var analogies =["une couleur","un livre","un dessin animé","un fruit","un lieu", "une oeuvre","un animal"];
+//var valeursAnalogies=["le bleu","les trois peits cochons","Le monde incroyable de gumball","la poire","un champs de paquerettes","Pecheur en mer de Williams turner","un poisson clown"]
+//var data={'une couleur':'le bleu','un livre':'les trois petits cochons','un dessin animé':'Le monde incroyable de Gumball','un fruit':'une poire','un lieu':'un champs de paquerettes','une oeuvre':'Pecheurs en mer de William Turner','un animal':'un poisson clown'}
+
+var data=[
+    {"analogie":"une couleur","valeur":"le bleu","explication":"La couleur bleu a une signification calme et apaisante, sans rajout. J'aimerais etre le bleu afin d'etre au plus proche de la fièrté humaine.","class":"bleu", "image":"img/bleu.jpg", "id":"Couleur"},
+
+    {"analogie":"un livre","valeur":"Et ils meurent tout les deux à la fin","explication":"Ce livre represente a mes yeux la tragedie dans tout ca cplendeur et image ce qu'est pour moi la vie. Ou tout est representer par du temps que nulle ne peut controler. Tout ce que l'on peut faire de ce temps c'est decider comment le dépenser.", "id":"Livre", "image":"img/mort.jpg"}, 
+
+    {"analogie":"un dessin animé","valeur":"le monde incroyable de gumball", "id":"Dessin-animé","image":"img/gumball.jpg", "explication":"Ce dessin animé represente pour moi la naiveté que le monde devrait incarné, j'aime la simplicité d'esprit que leur monde représente. J'espere que je pourrais amener ma réaliter a ressembler un peux plus au monde incroyable de Gumball."},
+
+    {"analogie":"un fruit","valeur":"la poire", "image":"img/poire.png", "id":"Fruit", "explication":"Les poires, avec leur forme délicate et leur teinte chatoyante, incarnent une splendeur naturelle qui émerveille les sens. Leurs courbes élégantes s'étirent avec grâce, tandis que leur peau lisse offre une palette de couleurs allant du vert tendre au rouge velouté. La chair juteuse à l'intérieur révèle une symphonie de saveurs sucrées, évoquant la douceur de l'été dans chaque bouchée. Les poires, joyaux de la nature, séduisent non seulement par leur apparence séduisante mais aussi par leur goût exquis, faisant de ces fruits une véritable œuvre d'art gustative. Et pour tout cela je suis la poire."}, 
+
+    {"analogie":"un lieu","valeur":"un champs de paquerettes","image":"img/Paquerette.jpg", "id":"Lieux", "explication":"Un champs de paquerette est calme, simple et present au quotidien lorsqu'on fait attention a lui. Un champs de paquerette est à mes yeux un lieu de recueille qui n'inspire qu'a la liberation de l'esprit, un chose que je recherche a tout moments dans mon quotidien, c'est pour cela que je fait toujours attention, aux paquerettes dans les herbes hates."}, 
+
+    {"analogie":"un Jeu vidéo","valeur":"Life is Strange", "id":"Jeu-vidéo", "image":"img/PAPILLON.jpg", "explication":"Life is strange est une aventure qui represente la vie autant dans sa beauté que dans sa triste réalité, j'ai été plus que marqué par life is strange, j'aime son atmosphere et je veux l'incarner et devenir aussi passionante que ses personnages."},
+    
+    {"analogie":"un animal","valeur":"un poisson clown", "id":"Animal", "image":"img/poissonClown.jpg", "explication":"Les poissons-clowns, avec leurs couleurs vives et leurs mimiques espiègles, ajoutent une touche de gaieté et d'amusement à l'univers sous-marin. Leurs rayures audacieuses et leurs nageoires ondulantes semblent dépeindre un tableau comique dans les eaux coralliennes. Ces petits acrobates aquatiques semblent presque jouer à cache-cache entre les tentacules des anémones de mer, créant un spectacle divertissant pour les observateurs sous-marins. Leurs interactions ludiques avec leurs congénères renforcent leur réputation de clowns des océans. À chaque mouvement, les poissons-clowns semblent répandre un éclat de rire silencieux parmi les coraux, apportant une joie indéniable à ceux qui ont la chance de les observer dans leur habitat marin. Je veux etre aussi drole et merveilleuse que ce poisson, je veux apporter cette meme lumiere sur terre que le poisson clown l'apporte sous la mer."}]
+
+
+    // photos poisson clown https://pixabay.com/fr/photos/poisson-eau-mer-poisson-clown-eau-2241634/ 
+    //poire https://pixabay.com/fr/vectors/poire-fruit-mod%C3%A8le-art-conception-6541074/ 
+    //bleu https://pixabay.com/fr/photos/mer-animal-m%C3%A9duse-animal-marin-21649/ 
+    //lis https://pixabay.com/fr/photos/bleu-mazarine-papillon-fleur-7753988/
+    // MORT https://pixabay.com/fr/photos/parc-banc-nuit-soir%C3%A9e-brouillard-3116883/
+    // PAQUERETTE https://pixabay.com/fr/photos/fleur-ros%C3%A9e-p%C3%A2querette-daisy-1144088/
+    // gumball https://pixabay.com/fr/illustrations/cat-poisson-vintage-curieux-4637643/
